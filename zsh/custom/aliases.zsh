fc684e803127b83a21dede7f717302ba2ac82670
Serdar Sutay <serdar@boundless.com>
2019-04-26 08:50:34 -0700

    Delete some unused code.

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -30,11 +30,6 @@ pgrefresh() {
   brew services restart postgresql
 }
 
-ctags_refresh() {
-  ctags -R .
-  ctags -R -f ./.git/tags
-}
-
 sslcert() {
   echo | openssl s_client -showcerts -servername $1 -connect $1:443 2>/dev/null | openssl x509 -inform pem -noout -text
 }
--- zsh/custom/exports.zsh
+++ zsh/custom/exports.zsh
@@ -28,9 +28,6 @@ export RESTIC_REPOSITORY="b2:dbalatero-backup"
 export SYNC_REPO="$RESTIC_REPOSITORY:/Sync"
 export FREEZE_REPO="$RESTIC_REPOSITORY:/Freeze"
 
-export GTAGSLABEL="ctags"
-export GTAGSCONF="$HOME/.globalrc"
-
 export FZF_DEFAULT_COMMAND='fd --type file --follow --hidden --exclude .git --exclude node_modules --color=always'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_DEFAULT_OPTS="--ansi"
