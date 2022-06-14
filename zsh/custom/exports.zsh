58428ff9cd5d595422979986638015b79e8ab246
David Balatero <dbalatero@gmail.com>
2022-06-14 10:49:13 -0400

    Fix colors

--- zsh/custom/exports.zsh
+++ zsh/custom/exports.zsh
@@ -6,7 +6,7 @@ export LC_CTYPE=en_US.UTF-8
 export DEVELOPMENT_DIRECTORY="$HOME/code"
 
 # FZF
-export FZF_DEFAULT_COMMAND="rg --files --color always --hidden --glob '!{node_modules/*,.git/*}'"
+export FZF_DEFAULT_COMMAND="rg --files --hidden --glob '!{node_modules/*,.git/*}'"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 # Editor
