f0be9c0f0847ffa12fc79e6f8d5ae40e78882818
David Balatero <dbalatero@gmail.com>
2022-07-18 14:25:56 -0400

    drop signal

--- hammerspoon/quick-switch.lua
+++ hammerspoon/quick-switch.lua
@@ -2,7 +2,6 @@ hyperKey
   :bind('1'):toApplication('/Applications/1Password 7.app')
   :bind('a'):toApplication('/Applications/Slack.app')
   :bind('c'):toApplication('/Applications/Google Chrome.app')
-  :bind('i'):toApplication('/Applications/Signal.app')
   :bind('l'):toApplication('/Users/dbalatero/Applications/Chrome Apps.localized/Google Calendar.app')
   :bind('s'):toApplication('/Applications/Spotify.app')
   :bind('t'):toApplication('/Applications/Alacritty.app')
