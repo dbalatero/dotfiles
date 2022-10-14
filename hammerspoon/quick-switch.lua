85f367a39b98c89806d934764c0506d188573ecd
David Balatero <dbalatero@users.noreply.github.com>
2022-10-14 07:50:17 -0400

    fix cal

--- hammerspoon/quick-switch.lua
+++ hammerspoon/quick-switch.lua
@@ -2,6 +2,6 @@ hyperKey
   :bind('1'):toApplication('/Applications/1Password 7.app')
   :bind('a'):toApplication('/Applications/Slack.app')
   :bind('c'):toApplication('/Applications/Google Chrome.app')
-  :bind('l'):toApplication('/Users/dbalatero/Applications/Chrome Apps.localized/Google Calendar.app')
+  :bind('l'):toApplication(os.getenv('HOME') .. '/Applications/Chrome Apps.localized/Google Calendar.app')
   :bind('s'):toApplication('/Applications/Spotify.app')
   :bind('t'):toApplication('/Applications/Alacritty.app')
