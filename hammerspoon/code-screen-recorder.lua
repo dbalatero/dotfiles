3e1242e1620e5c10c0f1e4ff5c5bf83057dd53dc
David Balatero <dbalatero@users.noreply.github.com>
2022-10-26 14:23:34 -0400

    First commit

---
+++ hammerspoon/code-screen-recorder.lua
@@ -0,0 +1,195 @@
+-- Our "movie script"
+local dummyScript = {
+  {
+    type = 'type',
+    text = "import EdgeImpulse from './index';"
+  },
+  { type = "newLine" },
+  { type = "newLine" },
+  {
+    type = 'type',
+    text = "const client = new EdgeImpulse({ apiKey: 'ei_yourkey' });"
+  },
+  { type = "newLine" },
+  { type = "newLine" },
+  {
+    type = 'type',
+    text = "const main = async () => {"
+  },
+  { type = "waitForEditor" },
+  { type = "newLine" },
+  {
+    type = 'type',
+    text = "const { projects } = await client.projects.",
+  },
+  { type = "wait", waitMs = 750 },
+  { type = "keyPress", keyName = "down", waitMs = 500 },
+  { type = "keyPress", keyName = "return", waitMs = 200 },
+  -- open parens
+  { type = "keyPress", modifiers = {'shift'}, keyName = "9", waitMs = 200 },
+  -- end of line
+  { type = "keyPress", modifiers = {'ctrl'}, keyName = "e", waitMs = 200 },
+  { type = "keyPress", keyName = ";" },
+  { type = "newLine" },
+  {
+    type = 'type',
+    text = "projects.forEach((project) => {",
+  },
+  { type = "newLine" },
+  {
+    type = 'type',
+    text = "console.log(`- Found project ${project.",
+  },
+  { type = "wait", waitMs = 1500 },
+  {
+    type = 'type',
+    text = "id",
+  },
+  { type = "wait", waitMs = 1500 },
+  { type = "keyPress", keyName = "return" },
+  {
+    type = 'type',
+    text = "}, description: ${project.desc",
+  },
+  { type = "wait", waitMs = 1500 },
+  { type = "keyPress", keyName = "return" },
+  -- end of line
+  { type = "keyPress", modifiers = {'ctrl'}, keyName = "e", waitMs = 200 },
+  {
+    type = 'type',
+    text = "`);",
+  },
+  { type = "keyPress", keyName = "down" },
+  { type = "keyPress", modifiers = {'ctrl'}, keyName = "e" },
+  { type = "keyPress", keyName = ";" },
+  { type = "newLine" },
+  { type = "keyPress", modifiers = {'cmd'}, keyName = "delete", waitMs = 200 },
+  {
+    type = 'type',
+    text = "};",
+  },
+  { type = "newLine" },
+  { type = "newLine" },
+  {
+    type = 'type',
+    text = "main();",
+  },
+}
+
+local function stringToChars(str)
+  local chars = {}
+  local current = 1
+
+  while current <= #str do
+    table.insert(chars, string.sub(str, current, current))
+    current = current + 1
+  end
+
+  return chars
+end
+
+local function eachCharacter(str, fn)
+  for _, char in ipairs(stringToChars(str)) do
+    fn(char)
+  end
+end
+
+local characterOverrides = {
+  [" "] = {{}, "space"},
+  ["!"] = {{'shift'}, '1'},
+  ["@"] = {{'shift'}, '2'},
+  ["#"] = {{'shift'}, '3'},
+  ["$"] = {{'shift'}, '4'},
+  ["%"] = {{'shift'}, '5'},
+  ["^"] = {{'shift'}, '6'},
+  ["&"] = {{'shift'}, '7'},
+  ["*"] = {{'shift'}, '8'},
+  ["("] = {{'shift'}, '9'},
+  [")"] = {{'shift'}, '0'},
+  ['"'] = {{'shift'}, "'"},
+  [":"] = {{'shift'}, ";"},
+  ["+"] = {{'shift'}, '='},
+  ["_"] = {{'shift'}, '-'},
+  ["<"] = {{'shift'}, ','},
+  [">"] = {{'shift'}, '.'},
+  ["{"] = {{'shift'}, '['},
+  ["}"] = {{'shift'}, ']'},
+  ["|"] = {{'shift'}, '\\'},
+  ["?"] = {{'shift'}, '/'},
+  ["~"] = {{'shift'}, '`'},
+}
+
+local actionHandlers = {
+  keyPress = function(action, nextAction)
+    local modifiers = action.modifiers or {}
+    hs.eventtap.keyStroke(modifiers, action.keyName, 0)
+
+    if action.waitMs then
+      hs.timer.doAfter(action.waitMs / 1000, nextAction)
+    else
+      nextAction()
+    end
+  end,
+  newLine = function(_action, nextAction)
+    hs.eventtap.keyStroke({}, "return", 0)
+    nextAction()
+  end,
+  type = function(action, nextAction)
+    local speed = action.speed or "fast"
+
+    if speed == "slow" then
+      local sleepMicroseconds = 20 * 1000
+
+      eachCharacter(action.text, function(char)
+        local character = char
+        local modifiers = {}
+
+        if characterOverrides[char] then
+          modifiers = characterOverrides[char][1] or {}
+          character = characterOverrides[char][2]
+        end
+
+        if char:find("[A-Z]") then
+          table.insert(modifiers, 'shift')
+        end
+
+        print("Doing: " .. inspect(modifiers) .. ", " .. character)
+
+        hs.eventtap.keyStroke(modifiers, character, 0)
+        -- hs.timer.usleep(sleepMicroseconds)
+      end)
+    else
+      hs.eventtap.keyStrokes(action.text)
+    end
+
+    nextAction()
+  end,
+  wait = function(action, nextAction)
+    hs.timer.doAfter(action.waitMs / 1000, nextAction)
+  end,
+  waitForEditor = function(_, nextAction)
+    hs.timer.doAfter(500 / 1000, nextAction)
+  end
+}
+
+local function runMovieScript(movieScript)
+  local currentIndex = 0
+  local nextAction
+
+  nextAction = function()
+    currentIndex = currentIndex + 1
+
+    local action = movieScript[currentIndex]
+
+    if action then
+      local handler = actionHandlers[action.type]
+      handler(action, nextAction)
+    end
+  end
+
+  nextAction()
+end
+
+hs.hotkey.bind(super, '0', function()
+  runMovieScript(dummyScript)
+end)
