0a36ea4ceb588c265220a9b887d22f6819dca837
David Balatero <dbalatero@users.noreply.github.com>
2023-02-02 10:44:48 -0500

    Only load Rust LSP when Cargo.toml is available

--- nvim/lua/config/lsp/support/rust.lua
+++ nvim/lua/config/lsp/support/rust.lua
@@ -1,6 +1,9 @@
 local lspconfig = require('lspconfig')
+local on_attach = require('config.lsp.shared').on_attach
 
 lspconfig.rust_analyzer.setup({
+  on_attach = on_attach,
+  root_dir = lspconfig.util.root_pattern('Cargo.toml'),
   settings = {
     ['rust-analyzer'] = {
       assist = {
