fd33f731b6a8902d2257ad772b5106cb4ff2d744
David Balatero <dbalatero@users.noreply.github.com>
2023-03-17 10:04:06 -0400

    Add experimental lsp flag

--- nvim/lua/config/lsp/support/sorbet.lua
+++ nvim/lua/config/lsp/support/sorbet.lua
@@ -1,5 +1,11 @@
 local lspconfig = require('lspconfig')
 
 lspconfig.sorbet.setup({
+  cmd = {
+    'srb',
+    'tc',
+    '--lsp',
+    '--enable-experimental-lsp-document-formatting-rubyfmt',
+  },
   root_dir = lspconfig.util.root_pattern('sorbet'),
 })
