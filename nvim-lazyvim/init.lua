43d367923a821b03ea1ba1eaacc6e7087bf5bc5d
David Balatero <dbalatero@users.noreply.github.com>
2023-03-25 18:54:59 -0400

    Remove lazyvim attempt

--- nvim-lazyvim/init.lua
+++
@@ -1,5 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
--- Enable line numbers
-vim.o.numbers = true
--- nvim-lazyvim/lua/config/options.lua
+++
@@ -1,5 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
-
--- Enable line numbers
-vim.o.numbers = true
