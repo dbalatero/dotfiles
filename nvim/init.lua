fb79cd54cd0baf58eeedd58449a3332869c39b14
David Balatero <dbalatero@users.noreply.github.com>
2023-03-27 10:12:00 -0400

    Remove wordwrap thing

--- nvim/init.lua
+++ nvim/init.lua
@@ -529,10 +529,6 @@ vim.keymap.set({ "n" }, "vv", "<C-w>v", { noremap = true })
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
--- Remap for dealing with word wrap
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
