99102bdb79d6628dcd79fe873dae65ec78d5481d
David Balatero <dbalatero@stripe.com>
2024-12-10 20:02:38 -0500

    ruby

--- lib/shared.sh
+++ lib/shared.sh
@@ -3,3 +3,25 @@ function command_exists() {
 
   command -v "$name" >/dev/null 2>&1
 }
+
+# _symlinks_current_dir="${BASH_SOURCE%/*}"
+
+# function dotfiles_location() {
+#   echo $(cd $_symlinks_current_dir/../.. && pwd)
+# }
+
+function dotfiles_location() {
+  echo "$HOME/.dotfiles"
+}
+
+function symlink_dotfile() {
+  local file=$1
+  local destination=$2
+  local full_file_path="$(dotfiles_location)/$file"
+
+  if [ ! -e "$destination" ]; then
+    echo "Symlinking $full_file_path -> $destination"
+    mkdir -p "$(dirname "$destination")"
+    ln -s "$full_file_path" "$destination"
+  fi
+}
