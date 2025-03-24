79a556641ea42d8d5adad3f8bc71c7612f89b8b1
David Balatero <dbalatero@users.noreply.github.com>
2025-03-24 12:38:15 -0400

    fix: trouble hotkeys with new trouble API change

--- nvim/lua/packages/lsp/init.lua
+++ nvim/lua/packages/lsp/init.lua
@@ -55,41 +55,40 @@ return {
         use_diagnostic_signs = true,
       })
 
+      vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", {
+        silent = true,
+        noremap = true,
+        desc = "Diagnostics (Trouble)",
+      })
       vim.keymap.set(
         "n",
-        "<leader>xx",
-        "<cmd>TroubleToggle<cr>",
-        { silent = true, noremap = true }
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        {
+          silent = true,
+          noremap = true,
+          desc = "Buffer Diagnostics (Trouble)",
+        }
       )
-      vim.keymap.set(
-        "n",
-        "<leader>xw",
-        "<cmd>TroubleToggle workspace_diagnostics<cr>",
-        { silent = true, noremap = true }
-      )
-      vim.keymap.set(
-        "n",
-        "<leader>xd",
-        "<cmd>TroubleToggle document_diagnostics<cr>",
-        { silent = true, noremap = true }
-      )
-      vim.keymap.set(
-        "n",
-        "<leader>xl",
-        "<cmd>TroubleToggle loclist<cr>",
-        { silent = true, noremap = true }
-      )
-      vim.keymap.set(
-        "n",
-        "<leader>xq",
-        "<cmd>TroubleToggle quickfix<cr>",
-        { silent = true, noremap = true }
-      )
+      vim.keymap.set("n", "<leader>xl", "<cmd>Trouble loclist toggle<cr>", {
+        silent = true,
+        noremap = true,
+        desc = "Location List (Trouble)",
+      })
+      vim.keymap.set("n", "<leader>xq", "<cmd>Trouble quickfix toggle<cr>", {
+        silent = true,
+        noremap = true,
+        desc = "Quickfix List (Trouble)",
+      })
       vim.keymap.set(
         "n",
         "gR",
-        "<cmd>TroubleToggle lsp_references<cr>",
-        { silent = true, noremap = true }
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        {
+          silent = true,
+          noremap = true,
+          desc = "LSP Definitions / references / ... (Trouble)",
+        }
       )
 
       -- More keybinds
