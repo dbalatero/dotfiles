cffe0f7a8aaa0fedc3c73ccb40bbd4c8aca3e74a
David Balatero <dbalatero@users.noreply.github.com>
2025-04-02 13:33:23 -0400

    feat: add quicker

--- nvim/lua/packages/editing.lua
+++ nvim/lua/packages/editing.lua
@@ -3,6 +3,42 @@
 --  ╰──────────────────────────────────────────────────────────╯
 return {
   {
+    "stevearc/quicker.nvim",
+    event = "FileType qf",
+    ---@module "quicker"
+    ---@type quicker.SetupOptions
+    config = function()
+      require("quicker").setup({
+        keys = {
+          {
+            ">",
+            function()
+              require("quicker").expand({
+                before = 2,
+                after = 2,
+                add_to_existing = true,
+              })
+            end,
+            desc = "Expand quickfix context",
+          },
+          {
+            "<",
+            function()
+              require("quicker").collapse()
+            end,
+            desc = "Collapse quickfix context",
+          },
+        },
+      })
+
+      vim.keymap.set("n", "<leader>q", function()
+        require("quicker").toggle()
+      end, {
+        desc = "Toggle quickfix",
+      })
+    end,
+  },
+  {
     "rmagatti/auto-session",
     lazy = false,
 
