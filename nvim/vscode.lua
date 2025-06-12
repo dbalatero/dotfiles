3d3663a7fd193d70960b353d865c075c7322d225
David Balatero <dbalatero@users.noreply.github.com>
2025-06-12 18:58:13 -0400

    fix: vscode vim setup

--- nvim/vscode.lua
+++ nvim/vscode.lua
@@ -1,1 +1,13 @@
+vim.api.nvim_create_user_command("TestConfig", function()
+  print("Config loaded successfully!")
+end, {})
+
 vim.opt.clipboard:append("unnamedplus")
+
+vim.keymap.set({ "n" }, "vv", function()
+  vim.cmd([[call VSCodeNotify('workbench.action.splitEditorRight')]])
+end)
+
+vim.keymap.set({ "n" }, "ss", function()
+  vim.cmd([[call VSCodeNotify('workbench.action.splitEditorDown')]])
+end)
