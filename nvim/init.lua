072f541a4d2e0b56f56eea50f994c4a39acad9dd
David Balatero <dbalatero@users.noreply.github.com>
2025-08-18 10:54:12 -0400

    markdown word wrap in neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -167,14 +167,17 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   pattern = "*",
 })
 
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-  pattern = "*.md",
-  command = "set wrap",
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.formatoptions:remove("t")
+  end,
 })
 
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
   pattern = "*.md",
-  command = "set formatoptions-=t",
+  command = "setlocal formatoptions-=t",
 })
 
 --  ╭──────────────────────────────────────────────────────────╮
